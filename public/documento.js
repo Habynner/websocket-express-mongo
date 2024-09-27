fe7409f5aaa9b73054b8ec3659b32bbed4994646
Habynner <habynner.jeuel@gmail.com>
2024-09-27 11:46:03 -0300

    cretion of websocket config, start websocket on front.

---
+++ public/documento.js
@@ -0,0 +1,12 @@
+import { emitirTextoEditor } from "./socket-front-document.js";
+
+const textoEditor = document.getElementById('editor-texto');
+textoEditor.addEventListener('keyup', () => {
+    emitirTextoEditor(textoEditor.value);
+  });
+
+  function atualizaTextoEditor(texto) {
+    textoEditor.value = texto;
+  }
+
+  export { atualizaTextoEditor };
